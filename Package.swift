211c14df2b2fe6b56a26887ce758dbe1be39283b
Dario Carlomagno <dario.carlomagno@gmail.com>
2019-07-31 17:38:44 +0100

    added platforms macos minimum requirement

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "WFAPPCore",
+    platforms: [
+        
+        // specify each minimum deployment requirement,
+        //otherwise the platform default minimum is used.
+        .macOS(.v10_13),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
