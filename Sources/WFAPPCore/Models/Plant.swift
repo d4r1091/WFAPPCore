e331b196c3cdca37682196a7c7b80d61457b73c2
Dario Carlomagno <dario.carlomagno@gmail.com>
2019-08-05 02:38:00 +0100

    added imageURL to the plant's model

--- Sources/WFAPPCore/Models/Plant.swift
+++ Sources/WFAPPCore/Models/Plant.swift
@@ -10,16 +10,33 @@ import Foundation
 
 public struct Plant: Decodable {
     
+    //MARK: Properties
+
     public var id: String?
     public var name: String?
+    public var imageURL: URL?
     public var attributes: [Attribute]?
     
+    //MARK: Initializers
+
     public init(id: String? = nil,
                 name: String? = nil,
+                imageURL: URL? = nil,
                 attributes: [Attribute]? = nil) {
         
         self.id = id
         self.name = name
+        self.imageURL = imageURL
         self.attributes = attributes
+    }
+    
+    //MARK: Codable
+    
+    private enum CodingKeys: String, CodingKey {
+        
+        case id
+        case name
+        case imageURL = "image_url"
+        case attributes
     }
 }
